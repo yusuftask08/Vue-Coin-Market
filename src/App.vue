f12f32da6f47ae6880bbbfa4aa2ca77511f3a847
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2021-04-30 15:34:06 +0300

    navbar added..

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <nav-bar> </nav-bar>
+    <transition name="fade" mode="out-in">
+      <router-view> </router-view>
+    </transition>
   </div>
 </template>
 
+<script>
+import navBar from "@/components/Home/navBar.vue";
+export default {
+  components: {
+    navBar,
+  },
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.fade-enter {
+  opacity: 0;
 }
-
-#nav {
-  padding: 30px;
+.fade-enter-active {
+  transition: opacity 0;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.fade-leave-enter {
+  transition: opacity 0.3s ease-in-out;
+  opacity: 0;
 }
 </style>
---
+++ src/components/Home/navBar.vue
@@ -0,0 +1,105 @@
+<template>
+  <nav
+    class="navbar navbar-expand-lg navbar-dark bg-dark navbar-linear"
+    :class="{ change_color: windowTop > 50 }"
+  >
+    <div class="container-fluid">
+      <a
+        class="navbar-brand"
+        href="/"
+        style="margin-right: 20px; margin-left: 50px"
+      >
+        <!-- <img
+          src="https://fontmeme.com/permalink/210430/fa46226807959f8c430111d46f2f464f.png"
+          alt="netflix-font"
+          border="0"
+          style="width: 150px"
+        /> -->
+        <img
+          src="https://fontmeme.com/permalink/210430/e2a4bcf352254f362ae0da7347cd4283.png"
+          alt="netflix-font"
+          style="width: 150px"
+          border="0"
+        />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Borsa</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Piyasa</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Yatırma</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Çekme</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <!-- <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Cüzdan</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Emirler </a>
+            </li>
+          </ul> -->
+          <button class="btn btn-outline-success" type="submit">
+            Giriş Yap
+          </button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      windowTop: 0,
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.onScroll);
+  },
+  beforeUnmount() {
+    window.removeEventListener("scroll", this.onScroll);
+  },
+  methods: {
+    onScroll(e) {
+      this.windowTop = e.target.documentElement.scrollTop; // scrool bar takip ediyorum ve 0 a bağlıyorum
+    },
+  },
+};
+</script>
+
+<style>
+.change_color {
+  background-color: rgb(20, 20, 20) !important;
+}
+.change_color_a {
+  color: #fff;
+}
+.nav-item a {
+  font-size: 15px;
+  font-weight: 500;
+  color: #000 !important;
+}
+.btn {
+  margin-right: 50px;
+}
+</style>
